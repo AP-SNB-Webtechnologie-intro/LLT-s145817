10137a5bc9bda75827fba2b848cc56f19e61c883
Jesse Boddaert <145460734+s145817@users.noreply.github.com>
2023-12-27 14:39:29 +0100

    contact pagina afgewerkt
    Opmaak beter gemaakt en adres gegevens toegevoegd van webdev en admin. Beide fictief uiteraard.

--- js/APOD.js
+++ js/APOD.js
@@ -1,25 +1,15 @@
-// URL van de NASA APOD API
+// URL van de NASA APOD API + API key ineens aan elkaar.
 let apiUrl = 'https://api.nasa.gov/planetary/apod?api_key=dObVVK1dLsMAYkW4gk2HX2SlHvqnM4ylAWLWdflQ';
 
-// Functie om de data van de NASA APOD API op te halen en weer te geven
-function fetchNasaApod() {
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Netwerk respons was niet ok');
-            }
-            return response.json(); 
-        })
-        .then(data => {
-            // Update de HTML-elementen met de titel en afbeelding
-            document.getElementById('apodTitle').textContent = data.title;
-            document.getElementById('apodImage').src = data.url;
-            document.getElementById('apodImage').alt = data.title;
-        })
-        .catch(error => {
-            console.error('Fout bij het ophalen van de NASA APOD data:', error);
-        });
-}
+fetch(apiUrl)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        // Update de HTML-elementen met de titel en afbeelding
+        document.getElementById('apodTitle').textContent = data.title;
+        document.getElementById('apodImage').src = data.url;
+        document.getElementById('apodImage').alt = data.title;
+        document.getElementById('apodText').textContent = data.explanation;
 
-// Roep de functie aan bij het laden van de pagina
-fetchNasaApod();
+    })
